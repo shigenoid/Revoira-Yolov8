0c0d6132024e8878c62e7243f10d5cf176132cd4
shigenoid <nasharuddinh7@gmail.com>
2025-03-08 02:32:45 +0000

    test

--- train.py
+++ train.py
@@ -3,18 +3,19 @@ import torch
 import os
 
 def main():
-    model = YOLO("runs/detect/bottle-1502/weights/last.pt")
+    model = YOLO("yolov8n.pt")
 
     # Train the model
     results = model.train(
         data="data.yaml",  # Path to your dataset configuration file
-        epochs=150,            # Number of training epochs
+        epochs=150, 
+        warmup_epochs=3,           # Number of training epochs
         batch=32,             # Batch size
         imgsz=640,            # Image size
         device="0",           # Use GPU (set to "cpu" if you don't have a GPU)
-        name="bottle-150",        # Name of the training run
+        name="revoira-150",        # Name of the training run
         workers=0,          # Disable multiprocessing (set to 0 for Windows)
-        resume=True
+        resume=False
     )
 
 if __name__ == "__main__":
